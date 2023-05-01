5ad436310dbe6e015d56c8792562b6481ee58399
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-05-01 12:22:36 +0600

    all the toast is added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({ quiz }) => {
 
     const handleQuizAnswer = (answer) => {
         if (correctAnswer !== answer) {
-            toast.error("Wrong", {
+            toast.error("Your are Wrong!", {
                 position: "top-center",
                 autoClose: 3000,
                 hideProgressBar: false,
@@ -20,7 +20,7 @@ const Card = ({ quiz }) => {
                 theme: "dark",
             });
         } else {
-            toast.success("Awesome that's right", {
+            toast.success("Awesome that's right!", {
                 position: "top-center",
                 autoClose: 3000,
                 hideProgressBar: false,
@@ -33,11 +33,24 @@ const Card = ({ quiz }) => {
         }
     }
 
+    const handleShowAnswer = () => {
+        toast(`${correctAnswer}`, {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
     return (
         <div className='text-center shadow-lg bg-yellow-50 mb-12 w-11/12 md:w-3/4 p-6 mx-auto rounded-md'>
             <div className='relative'>
                 <h4 className='text-2xl font-semibold text-yellow-500 mr-3 mt-2'>Quiz: {question}</h4>
-                <button className='text-2xl text-yellow-400 opacity-75 hover:opacity-100  absolute -top-4 -right-2'><FaEye /> </button>
+                <button onClick={handleShowAnswer} className='text-2xl text-yellow-400 opacity-75 hover:opacity-100  absolute -top-4 -right-2'><FaEye /> </button>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-6">
                 {
